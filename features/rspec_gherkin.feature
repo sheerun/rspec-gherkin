02ad08990179055103c0a6df5a4af929b1a31cc7
Dane Andersen <dane.andersen@gmail.com>
2015-01-16 08:30:10 -0500

    Moved files into proper locations and started changing an acceptance test written in rspec to being written to use rspec gherkin.

---
+++ features/rspec_gherkin.feature
@@ -0,0 +1,6 @@
+Feature: RSpec gherkin test runner
+  As an acceptance tester of software
+  I want to run specs tied to gherkin
+  In order to validate that acceptance criteria have been met
+
+  Scenario:
--- spec/features/rspec_gherkin_spec.rb
+++ spec/features/rspec_gherkin_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe 'The CLI', :type => :integration do
+feature 'RSpec gherkin test runner' do
   context 'running features from features directory' do
 
-    it 'ignores --tag ~feature flag when running features' do
+    scenario 'ignores --tag ~feature flag when running features' do
       expect(%x(rspec features --tag ~feature 2>&1)).to include('9 examples, 1 failure, 3 pending')
     end
 
-    it 'ignores --tag ~type:feature flag when running features' do
+    scenario 'ignores --tag ~type:feature flag when running features' do
       expect(%x(rspec features --tag ~type:feature 2>&1)).to include('9 examples, 1 failure, 3 pending')
     end
   end
@@ -17,52 +17,52 @@ describe 'The CLI', :type => :integration do
       @result = %x(rspec --tag feature --format documentation 2>&1)
     end
 
-    it 'passes all specs' do
+    scenario 'passes all specs' do
       expect(@result).to include('9 examples, 1 failure, 4 pending')
     end
 
-    it 'prepends features with "Feature: " prefix' do
+    scenario 'prepends features with "Feature: " prefix' do
       expect(@result).to include('Feature: A simple feature')
     end
 
-    it 'prepends scenarios with "Scenario: " prefix' do
+    scenario 'prepends scenarios with "Scenario: " prefix' do
       expect(@result).to include('Scenario: A simple scenario')
     end
 
     context 'non-existing scenario' do
-      it 'shows name of non-existing scenario' do
+      scenario 'shows name of non-existing scenario' do
         expect(@result).to include("Scenario: Non-existing scenario")
       end
 
-      it 'shows that spec implements non-existing scenario' do
+      scenario 'shows that spec implements non-existing scenario' do
         expect(@result).to include("No such scenario in 'features/no_scenario.feature'")
       end
 
-      it 'shows line number where missing scenario is mentioned' do
+      scenario 'shows line number where missing scenario is mentioned' do
         expect(@result).to include("/spec/features/no_scenario_spec.rb:2")
       end
     end
 
     context 'non-existing feature' do
-      it 'shows name of non-existing feature' do
+      scenario 'shows name of non-existing feature' do
         expect(@result).to include("Feature: Missing feature")
       end
 
-      it 'shows that spec implements non-existing feature' do
+      scenario 'shows that spec implements non-existing feature' do
         expect(@result).to include("No such feature in 'features/no_feature.feature'")
       end
 
-      it 'shows line number where missing scenario is mentioned' do
+      scenario 'shows line number where missing scenario is mentioned' do
         expect(@result).to include("/spec/features/no_feature_spec.rb:3")
       end
     end
 
     context 'updated features and scenarios' do
-      it 'recognizes and notifies when feature is marked as @updated' do
+      scenario 'recognizes and notifies when feature is marked as @updated' do
         expect(@result).to include('Feature has been marked as updated')
       end
 
-      it 'recognizes and notifies when scenario is marked as @updated' do
+      scenario 'recognizes and notifies when scenario is marked as @updated' do
         expect(@result).to include('Scenario has been marked as updated')
       end
     end
