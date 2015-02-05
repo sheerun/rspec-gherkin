887c7cccfb2f5c2d4ab1992c881f64aea017a091
Alan Guy <Alan.L.Guy@gmail.com>
2015-02-04 22:28:02 -0500

    - Allow JRuby failures until we officially support it - Actually fix Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.root(File.expand_path(File.dirname(__FILE__) + '/..'))
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/features/'
+end
+
 
 require 'rspec-gherkin'
